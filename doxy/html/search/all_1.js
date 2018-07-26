8fb926fdbee1dbdf9e9d40fbc5dd5c4d53762940
Sudhanva <nsudhanva@gmail.com>
2018-07-26 20:59:30 +0530

    Doxygen

---
+++ doxy/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['using_20machine_20learning_2fdeep_20learning_20to_20identify_20home_20appliances_20consuming_20excessive_20power',['Using Machine Learning/Deep Learning to identify home appliances consuming excessive power',['../md__d_1__code__fast_stream_tech_home__r_e_a_d_m_e.html',1,'']]]
+];
